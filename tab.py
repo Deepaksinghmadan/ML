308c42e743f257fcb00eac6bbcf16619837ab7fc
Deepak <deepakms.real@gmail.com>
2018-01-31 00:37:22 +0530

    Add files via upload

---
+++ tab.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 00:33:20 2018
+
+@author: deepak
+"""
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
