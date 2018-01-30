7721d136c42a41b11f074cd1af87f803f2647a58
Deepak <deepakms.real@gmail.com>
2018-01-31 00:44:15 +0530

    Add files via upload

---
+++ version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 00:39:02 2018
+
+@author: deepak
+"""
+
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
