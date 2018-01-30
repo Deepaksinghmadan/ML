76ae1d340a1f83786b875c754ef3eb679d86f083
Deepak <deepakms.real@gmail.com>
2018-01-31 00:44:44 +0530

    Add files via upload

---
+++ accept input.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 00:42:51 2018
+
+@author: deepak
+"""
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
---
+++ datetime.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 00:41:19 2018
+
+@author: deepak
+"""
+
+import datetime
+n = datetime.datetime.now()
+print ("Current date and time : ")
+print (n.strftime("%Y-%m-%d %H:%M:%S"))
