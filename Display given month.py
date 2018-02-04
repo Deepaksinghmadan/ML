59138f5a8e29f83f75c15df9e8ea961583ac35c9
Deepak <deepakms.real@gmail.com>
2018-02-05 00:43:43 +0530

    Add files via upload

---
+++ Display given month.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 00:37:42 2018
+
+@author: deepak
+"""
+
+import calendar
+y = int(input("Input the year : "))
+m = int(input("Input the month : "))
+print(calendar.month(y, m))
