5b7650f1198bd0bf57c50f07e52a3c7ac63899a1
Deepak <deepakms.real@gmail.com>
2018-02-05 00:43:14 +0530

    Add files via upload

---
+++ Print multiline sring.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 00:39:56 2018
+
+@author: deepak
+"""
+
+print("""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
