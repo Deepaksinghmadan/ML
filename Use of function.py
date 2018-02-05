7fd712fda138011b253e84829ff2c8e37948df74
Deepak <deepakms.real@gmail.com>
2018-02-05 21:20:22 +0530

    Add files via upload

---
+++ Use of function.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 20:42:09 2018
+
+@author: deepak
+"""
+
+def logical(n):
+    if n > 17:
+        return((n-17)*2)
+    else:
+        return(17-n)
+a= logical(25)
+b = logical(14)
